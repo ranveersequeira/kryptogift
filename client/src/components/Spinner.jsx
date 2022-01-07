a52a7f4d2c4b7d60ecb40dc5bf49ceb8ea574caa
RanveerSequeira <ranveersequeira@gmail.com>
2022-01-07 19:55:45 +0530

    CRA

--- client/src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Spinner = () => {
-    return (
-        <h1>Spinner</h1>
-    )
-}
-
-export default Spinner
--- client/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Loader } from './Loader'
-export { default as Welcome } from './Welcome'
-export { default as Footer } from './Footer'
-export { default as Services } from './Services'
-export { default as Spinner } from './Spinner'
-export {default as Transactions} from './Transactions'
